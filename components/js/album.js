3898986812d63f7c455d8f9ddfc6d26e51db65d4
Adnard-kod <andra.lally@gmail.com>
2015-07-21 20:44:43 -0500

    add handle handlebars logic

--- components/js/album.js
+++ components/js/album.js
@@ -1,9 +1,13 @@
 document.addEventListener("DOMContentLoaded",function(){
   
   function Album() {
-    this.photos = document.getElementById('theList');
-    this.albumList = 'albumList'
-    this.clearAllBtn = document.getElementById('clearAll')
+    this.newPhotoItem = null;
+    this.newPhotoList = true;
+    this.photosList = $('#theList');
+    this.albumList = 'albumList';
+    this.form = $('form');
+    this.clearAllBtn = $('#clearAll');
+    this.photoTmp = $('#photo-temp').html();
   }
 
   Album.prototype = {
@@ -14,14 +18,31 @@ document.addEventListener("DOMContentLoaded",function(){
     loadAlbum: function() {
       var list = localStorage.getItem(this.albumList)
       if(list) {
-        this.photos.innerHTML = list;
+        var photoUrl = $('#photoItem')
       }
     }, 
     bindEvents: function() {
+      this.form.on('submit', function(e) {
+        this.addPhoto(e)
+      }.bind(this))
 
-      this.clearAllBtn.addEventListener('click', function(e) {
+      this.clearAllBtn.on('click', function(e) {
         this.clearAll(e);
-      }.bind(this),false)
+      }.bind(this))
+    },
+    addPhoto: function(e) {
+      e.preventDefault();
+      var img = $('form #photoItemImg').val()
+      var title = $('form #photoItemTitle').val()
+      var photo = {"img": img, 'title': title}
+      var photo = this.buildPhoto(photo)
+      this.photosList.append(photo)
+      this.resetForm()
+    },
+    buildPhoto: function(val) {
+      var source = this.photoTmp
+      var template = Handlebars.compile(source)
+      return template(val);
     },
     save: function(e) {
       e.preventDefault();
@@ -31,9 +52,17 @@ document.addEventListener("DOMContentLoaded",function(){
       e.preventDefault();
       localStorage.clear()
       location.reload();
+    },
+    resetForm: function() {
+      $('form')[0].reset()
+      $('#photoItem').focus()
+      $('.sortable').sortable('destroy')
+      $('.sortable').sortable({
+        handle: '.handle'
+      });
     }
   };
 
   var newAlbum = new Album;
   newAlbum.init();
-}, false);
+});
